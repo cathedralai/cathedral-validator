ac3c561cd2fd92c0bd74f65a0bf3f194bbf22e37
Fred E <7602667+wallscaler@users.noreply.github.com>
2026-07-27 10:48:08 -0400

    chore(manifest): the status timer matches upstream, do not declare it

--- tools/manifest.sh
+++ tools/manifest.sh
@@ -46,8 +46,7 @@ deploy/sn39/cathedral-sn39-public-status.service \
 config/validator-mainnet-sn39-launch.toml \
 deploy/sn39/cathedral-sn39-release-launcher.py \
 deploy/sn39/cathedral-validator-sn39-launch.service \
-deploy/sn39/cathedral-validator-sn39-reconcile.service \
-deploy/sn39/cathedral-sn39-public-status.timer"
+deploy/sn39/cathedral-validator-sn39-reconcile.service"
 
 is_allowed_divergence() {
   local needle="$1" p
